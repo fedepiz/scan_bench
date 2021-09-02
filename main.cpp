30074670dc0233c406fee8f7b3cf9cba35ef3cc3
Federico Pizzuti <pizzutifederico27@gmail.com>
2021-09-02 17:17:52 +0100

    Removed VCPKG dependency

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <optional>
 
-#include <argumentum/argparse.h>
+//#include <argumentum/argparse.h>
 
 #include <common.h>
 #include <test.h>
@@ -85,7 +85,7 @@ constexpr int DEFAULT_NUM_STEPS = 64;
 constexpr int DEFAULT_RUNS_PER_STEP = 10;
 
 bool handle_arguments(conf& conf, int argc, char** argv) {
-    using namespace argumentum;
+    //using namespace argumentum;
 
     std::optional<int> step_size_param;
     std::optional<int> num_steps_param;
@@ -94,60 +94,60 @@ bool handle_arguments(conf& conf, int argc, char** argv) {
 
     conf.step_size = DEFAULT_STEP_SIZE;
 
-    auto parser = argument_parser();
-    auto params = parser.params();
-    parser.config().program(argv[0]).description("Scan experimental bench");
-    params.add_parameter(step_size_param, "--step_size", "-s").nargs(1).help("Input increment per step size");
-    params.add_parameter(num_steps_param, "--num_steps", "-n").nargs(1).help("Total number of steps");
-    params.add_parameter(runs_per_step_param, "--runs", "-r").nargs(1).help("Repeat runs per step");
-    params.add_parameter(gold_check_param, "--gold", "-g").nargs(0).help("Enable gold checking (use only with small inputs)");
+    // auto parser = argument_parser();
+    // auto params = parser.params();
+    // parser.config().program(argv[0]).description("Scan experimental bench");
+    // params.add_parameter(step_size_param, "--step_size", "-s").nargs(1).help("Input increment per step size");
+    // params.add_parameter(num_steps_param, "--num_steps", "-n").nargs(1).help("Total number of steps");
+    // // params.add_parameter(runs_per_step_param, "--runs", "-r").nargs(1).help("Repeat runs per step");
+    // // params.add_parameter(gold_check_param, "--gold", "-g").nargs(0).help("Enable gold checking (use only with small inputs)");
 
-    if (!parser.parse_args(argc, argv, 1))
-        return false;
+    // if (!parser.parse_args(argc, argv, 1))
+    //     return false;
 
     conf.step_size = DEFAULT_STEP_SIZE;
-    if (step_size_param == std::nullopt) {
-        std::cout << "Using default step size: " << DEFAULT_STEP_SIZE << std::endl;
-    } else {
-        conf.step_size = *step_size_param;
-        if (conf.step_size < MIN_DIVIDER || conf.step_size % MIN_DIVIDER != 0) {
-            std::cout << "Invalid step size: " << conf.step_size << " must be divisible by " << MIN_DIVIDER << std::endl;
-            return false;
-        }
-    }
+    // if (step_size_param == std::nullopt) {
+    //     std::cout << "Using default step size: " << DEFAULT_STEP_SIZE << std::endl;
+    // } else {
+    //     conf.step_size = *step_size_param;
+    //     if (conf.step_size < MIN_DIVIDER || conf.step_size % MIN_DIVIDER != 0) {
+    //         std::cout << "Invalid step size: " << conf.step_size << " must be divisible by " << MIN_DIVIDER << std::endl;
+    //         return false;
+    //     }
+    // }
 
     conf.num_sizes = DEFAULT_NUM_STEPS;
-    if (num_steps_param == std::nullopt) {
-        std::cout << "Using default number of steps: " << conf.num_sizes << std::endl;
-    }
-    else {
-        conf.num_sizes = *num_steps_param;
-        if (conf.num_sizes < 0) {
-            std::cout << "Invalid number of steps: " << conf.num_sizes << " must be a positive integer" << std::endl;
-            return false;
-        }
-    }
+    // if (num_steps_param == std::nullopt) {
+    //     std::cout << "Using default number of steps: " << conf.num_sizes << std::endl;
+    // }
+    // else {
+    //     conf.num_sizes = *num_steps_param;
+    //     if (conf.num_sizes < 0) {
+    //         std::cout << "Invalid number of steps: " << conf.num_sizes << " must be a positive integer" << std::endl;
+    //         return false;
+    //     }
+    // }
 
     conf.run_per_size = DEFAULT_RUNS_PER_STEP;
-    if (runs_per_step_param == std::nullopt) {
-        std::cout << "Using default number of runs per step: " << conf.run_per_size << std::endl;
-    }
-    else {
-        conf.run_per_size = *runs_per_step_param;
-        if (conf.run_per_size < 1) {
-            std::cout << "Invalid number of runs per step: " << conf.run_per_size << " must be > 0" << std::endl;
-            return false;
-        }
-    }
+    // if (runs_per_step_param == std::nullopt) {
+    //     std::cout << "Using default number of runs per step: " << conf.run_per_size << std::endl;
+    // }
+    // else {
+    //     conf.run_per_size = *runs_per_step_param;
+    //     if (conf.run_per_size < 1) {
+    //         std::cout << "Invalid number of runs per step: " << conf.run_per_size << " must be > 0" << std::endl;
+    //         return false;
+    //     }
+    // }
 
     conf.gold_check = gold_check_param;
-    const char *status;
-    if (conf.gold_check) {
-        status = "enabled";
-    } else {
-        status = "disabled";
-    }
-    std::cout << "Gold check is: " << status << std::endl;
+    // const char *status;
+    // if (conf.gold_check) {
+    //     status = "enabled";
+    // } else {
+    //     status = "disabled";
+    // }
+    // std::cout << "Gold check is: " << status << std::endl;
 
     return true;
 }
@@ -175,6 +175,7 @@ int main(int argc, char** argv) {
         lines.reserve(lines.size() + result.size());
         std::move(std::begin(result), std::end(result), std::back_inserter(lines));
     }
+
 
      // printout results as csv to stdout
     for (const auto& line:lines) {
